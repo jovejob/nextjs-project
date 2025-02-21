86cb4bdb856b8170e25e85dd6e3680fcedc0e03b
Jove Trajkoski <jovetrajkoskidev@gmail.com>
2025-02-21 12:28:13 +0100

    Polish Second card

--- app/components/SecondaryCard.tsx
+++ app/components/SecondaryCard.tsx
@@ -5,7 +5,6 @@ import LinkButton from './LinkButton';
 interface SecondaryCardProps {
   title: string;
   description: string;
-  image?: string;
   buttonText?: string;
   buttonLink?: string;
 }
@@ -13,20 +12,14 @@ interface SecondaryCardProps {
 const SecondaryCard: React.FC<SecondaryCardProps> = ({
   title,
   description,
-  image,
   buttonText,
   buttonLink,
 }) => {
   return (
     <div className={styles.secondaryCard}>
-      {image && (
-        <div className={styles.imageContainer}>
-          <img src={image} alt={title} />
-        </div>
-      )}
       <div className={styles.content}>
-        <h3>{title}</h3>
-        <p>{description}</p>
+        <h3 className={styles.heading}>{title}</h3>
+        <p className={styles.description}>{description}</p>
         {buttonText && buttonLink && (
           <LinkButton
             text={buttonText}
@@ -41,6 +34,50 @@ const SecondaryCard: React.FC<SecondaryCardProps> = ({
 };
 
 export default SecondaryCard;
+
+// import React from 'react';
+// import styles from '@styles/SecondaryCard.module.scss';
+// import LinkButton from './LinkButton';
+
+// interface SecondaryCardProps {
+//   title: string;
+//   description: string;
+//   image?: string;
+//   buttonText?: string;
+//   buttonLink?: string;
+// }
+
+// const SecondaryCard: React.FC<SecondaryCardProps> = ({
+//   title,
+//   description,
+//   image,
+//   buttonText,
+//   buttonLink,
+// }) => {
+//   return (
+//     <div className={styles.secondaryCard}>
+//       {image && (
+//         <div className={styles.imageContainer}>
+//           <img src={image} alt={title} />
+//         </div>
+//       )}
+//       <div className={styles.content}>
+//         <h3>{title}</h3>
+//         <p>{description}</p>
+//         {buttonText && buttonLink && (
+//           <LinkButton
+//             text={buttonText}
+//             link={buttonLink}
+//             bgColor="#F0F2EC"
+//             textColor="#2E2E2E"
+//           />
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SecondaryCard;
 
 // import React from 'react';
 // import styles from '@styles/SecondaryCard.module.scss';
